6b07ecac0fd797df80bea39202f48c6168e02f40
m13 <mimmo@linino.org>
2014-05-22 23:25:32 +0200

    first commit

---
+++ include/config.h
@@ -0,0 +1,9 @@
+#define CONFIG_AR7240 1
+#define CONFIG_MACH_HORNET 1
+#define CONFIG_HORNET_1_1_WAR 1
+#define NEW_DDR_TAP_CAL 1
+#define CONFIG_LININO_DDR2 1
+#define FLASH_SIZE 16
+
+/* Automatically generated - do not edit */
+#include <configs/ap121.h>
---
+++ include/version_autogenerated.h
@@ -0,0 +1,1 @@
+#define U_BOOT_VERSION "U-Boot 1.1.4-linino-gd3d515d9-dirty"
